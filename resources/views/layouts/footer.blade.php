215b9f0299349b20a9942dc13dd5d3d02e2b44b3
President Raindas <presidentraindas@gmail.com>
2022-01-27 23:55:44 +0100

    Initial commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="bg-gray-800 py-20 mt-20">
+    <div class="sm:grid grid-cols-3 w-25 pb-10 m-auto border-b-2 border-gray-700">
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Pages
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                
+            </ul>
+        </div>
+    </div>
+</footer>
