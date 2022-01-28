3ee40ee94ea5143cd292ebbaf576c091def9542b
President Raindas <presidentraindas@gmail.com>
2022-01-28 02:43:33 +0100

    view and style changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,16 @@
-HOME
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="background-image grid grid-cols-1 m-auto">
+        <div class="flex text-gray-100 pt-10">
+            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
+                <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
+                    Do you want to become a developer?
+                </h1>
+                <a href="/blog" class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">Read More</a>
+            </div>
+        </div>
+    </div>
+
+@endsection
