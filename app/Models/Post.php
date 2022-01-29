2d537b1b9f97510d8b65c024d974c57251e0ee2c
President Raindas <presidentraindas@gmail.com>
2022-01-29 12:39:50 +0100

    new components

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,12 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
     use HasFactory;
+    use Sluggable;
+
+    protected $fillable = ['title', 'slug', 'description', 'image_path', 'user_id'];
 
     public function user() {
         return $this->belongsTo(User::class);
+    }
+
+    public function sluggable(): array {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -10,6 +10,15 @@
         </div>
     </div>
 
+    @if (session()->has('message'))
+        <div class="w-4/5 m-auto mt-10 pl-2">
+            <p class="w-2/6 mb-4 text-gray-50 bg-green-500 rounded-2xl py-4">
+                {{ session()->get('message') }}
+            </p>
+        </div>
+    @endif
+
+
     @if (Auth::check())
         <div class="pt-15 w-4/5 m-auto">
             <a href="/blog/create" class="bg-blue-500 uppercase bg-transparent text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
@@ -21,7 +30,7 @@
     @foreach ($posts as $post)
         <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
             <div>
-                <img src="https://cdn.pixabay.com/photo/2016/11/29/06/18/home-office-1867761_1280.jpg" alt="">
+                <img src="{{ asset('images/'.$post->image_path) }}" alt="">
             </div>
             <div>
                 <h2 class="text-gray-700 font-bold text-5xl pb-4">
@@ -39,6 +48,26 @@
                 <a href="/blog/{{ $post->slug }}" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
                     Keep Reading
                 </a>
+
+                @if (isset(Auth::user()->id) && Auth::user()->id == $post->user_id)
+                    <span class="float-right">
+                        <a href="/blog/{{ $post->slug }}/edit" class="text-gray-700 italic hover:text-gray-900 pb-1 border-b-2">
+                            Edit
+                        </a>
+                    </span>
+
+                    <span class="float-right">
+                        <form action="/blog/{{ $post->slug }}" method="POST">
+                            @csrf
+                            @method('delete')
+
+                            <button class="text-red-500 pr-3" type="submit">
+                                Delete
+                            </button>
+
+                        </form>
+                    </span>
+                @endif
             </div>
         </div> 
     @endforeach
